f801160f4cedf8ac6152ec7e779c0b2749e18a07
dancheg97 <dancheg97@fmnx.su>
2023-04-11 16:39:06 +0700

    added installtion stuff

--- lib/screens/installation.dart
+++ lib/screens/installation.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:installer/components/buttons.dart';
+import 'package:installer/constants.dart';
+import 'package:installer/screens/language.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:window_manager/window_manager.dart';
 
-class InstallationContent extends StatelessWidget {
+class InstallationContent extends StatefulWidget {
   const InstallationContent({super.key});
 
   @override
+  State<InstallationContent> createState() => _InstallationContentState();
+}
+
+class _InstallationContentState extends State<InstallationContent> {
+  cleanPrefs() async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    cleanPrefs();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/fleuos.png",
+              height: MediaQuery.of(context).size.height * 0.36,
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.65,
+              child: const Text(
+                "Welcome to FleuOS",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.65,
+              child: const Text(
+                "You can test the system on this live ISO image, or run installation process.",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FleuTextButton(
+                  text: "Test system",
+                  onPressed: () {
+                    windowManager.destroy();
+                  },
+                ),
+                const SizedBox(width: 42),
+                FleuTextButton(
+                  text: "Install",
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LanguageContent(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
