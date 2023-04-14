e9b3e78598bf91fd937fa8b8356b78462c8d15b0
dancheg97 <dancheg97@fmnx.su>
2023-04-14 09:31:03 +0300

    Update 'lib/utils/install.dart'

--- lib/utils/install.dart
+++ lib/utils/install.dart
@@ -179,6 +179,7 @@ Future<String> installSystem() async {
     "archinstall --silent --disk_layouts disk.json --config config.json --creds creds.json",
     "cp /etc/os-release /mnt/archinstall/etc/os-release",
     "cp /etc/pacman.conf /mnt/archinstall/etc/pacman.conf",
+    "mkdir -p /mnt/archinstall/etc/dconf/profile",
     "cp /etc/dconf/profile/user /mnt/archinstall/etc/dconf/profile/user",
     "mkdir -p /mnt/archinstall/etc/dconf/db/local.d",
     "cp /etc/dconf/db/local.d/00-settings /mnt/archinstall/etc/dconf/db/local.d/00-settings",
