82ce6fc8dcd7c273abe7b1443e5577f949ec1eae
USERNAME <EMAIL>
2023-04-16 06:55:53 +0700

    v0.5
    Former-commit-id: b7be6d1f2e5e3f3d00a1086d64bf56db280af43c

--- update.sh
+++ update.sh
@@ -10,9 +10,14 @@ mv fmnx-install-v$newver.tar.gz fmnx-install
 cd fmnx-install
 
 tarsum=$(sha1sum  fmnx-install-v$newver.tar.gz)
-echo $tarsum
-read tarsum
-awk "{ if (NR == 30) print "$tarsum"; else print $0}" PKGBUILD > PKGBUILD
+sha1sum  $tarsum > tmpsum
+sed -i 's|  fmnx-install.sh||g' tmpsum
+sed -i 's|\r\n||g' tmpsum
+newsum=$(cat tmpsum)
+rm tmpsum
+
+sed -i "s|$sha1sums|$newsum|g"
+
 
 git add .
 git commit -m v$newver
